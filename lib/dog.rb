bb90af22f638fa50574a26767d93f5e35329de04
Erin Walker <erinb0317@hotmail.com>
2018-01-13 03:07:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
    def name
      @name
-   end 
+   end
    def name=(new_name)
      @name = new_name
    end
    def breed
      @breed
-   end 
+   end
    def breed=(new_breed)
      @breed = new_breed
    end
- end  
+ end
