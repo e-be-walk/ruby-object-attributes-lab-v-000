704704838e8c93b842c77bb760e1891e9bcaae62
Erin Walker <erinb0317@hotmail.com>
2018-01-13 02:39:48 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,18 @@
 class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+
+  def dog_type=(name_breed)
+    name, breed = name_breed.split
+    @name = name
+    @breed = breed
+  end
+
+  def dog_type
+    "#{@name} #{@breed}".strip
+  end
+
 end
+
