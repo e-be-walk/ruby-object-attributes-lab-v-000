590866d2c6d6832226ed263430c2acf5b3662844
Erin Walker <erinb0317@hotmail.com>
2018-01-13 02:36:54 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def dog_type=(name_breed)
+    name, breed = name_breed.split
+    @name = name
+    @breed = breed
+  end
+
+  def dog_type
+    "#{@name} #{@breed}".strip
+  end
+
+end
+
+fido = Dog.new("Fido", " ")
+snoopy = Dog.new("Snoopy", "Beagle")
