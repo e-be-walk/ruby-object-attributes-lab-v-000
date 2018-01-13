f85c355f4aa9b32aad2672f3371112ae65269484
Erin Walker <erinb0317@hotmail.com>
2018-01-13 03:07:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,14 @@
 class Dog
-
-  attr_accessor :name, :breed
-
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
-
-  def dog_type=(name_breed)
-    name, breed = name_breed.split
-    @name = name
-    @breed = breed
-  end
-
-  def display
-    puts "I am #{@name}, I am a #{@breed} dog."
-  end
-
-end
-
-fido = Dog.new('Fido', ' ')
-puts fido.inspect
-snoopy = Dog.new("Snoopy", "Beagle")
+   def name
+     @name
+   end 
+   def name=(new_name)
+     @name = new_name
+   end
+   def breed
+     @breed
+   end 
+   def breed=(new_breed)
+     @breed = new_breed
+   end
+ end  
